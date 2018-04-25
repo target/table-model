ca1b18c7025e73e048832fa93e5f6d76e6f7b907
Edward Nicholes Jr <GameDevFox@gmail.com>
2018-04-24 21:49:25 -0500

    Add link to repo in demo

--- src/demo/demo.js
+++ src/demo/demo.js
@@ -3,7 +3,7 @@ import Grid from './grid';
 
 const Demo = () => (
   <div className="demo">
-    <h1>TableModel ag-Grid Demo</h1>
+    <h1>TableModel ag-Grid Demo - <a href="https://github.com/target/table-model">https://github.com/target/table-model</a></h1>
     <Grid/>
   </div>
 );
